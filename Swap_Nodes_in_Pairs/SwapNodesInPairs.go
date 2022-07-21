dac5958a074c99d9fbe074c8ab6b6fdf25a8d4a1
Tristone <trist725@live.cn>
2022-07-21 22:51:20 +0800

    24. 两两交换链表中的节点

---
+++ Swap_Nodes_in_Pairs/SwapNodesInPairs.go
@@ -0,0 +1,38 @@
+package Swap_Nodes_in_Pairs
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// 24
+// https://leetcode.cn/problems/swap-nodes-in-pairs/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	var (
+		dummy = &ListNode{Next: head}
+		cur   = dummy
+	)
+
+	for cur.Next != nil && cur.Next.Next != nil {
+		n1 := cur.Next
+		n2 := n1.Next
+		n3 := n2.Next
+
+		cur.Next = n2
+		n2.Next = n1
+		n1.Next = n3
+
+		cur = n2
+	}
+
+	return dummy.Next
+}
